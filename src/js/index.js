05b20b9eb8581bd2a755377518a3ba42fa42782d
remi208 <s208@tmm-sapr.org>
2020-12-21 16:52:19 +0200

    service page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
 // @flow
 import './markup-menu';
+import $ from 'jquery';
+import 'slick-carousel';
 import svgLoader from './svg-loader';
 
 document.addEventListener('DOMContentLoaded', () => {
   svgLoader();
+
+  const slider = $('.service__slider');
+
+  if (slider.length) {
+    slider.slick({
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      centerPadding: '100px',
+    });
+  }
 });
