231e48ea45f8a0dfb8d64e82e6fa45df1f23ace4
remi208 <s208@tmm-sapr.org>
2020-12-27 19:58:58 +0200

    responsive tablet service page

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 
 export default () => {
   const trigger = $('.menu-trigger, .menu-close');
+  const wrapper = $('.wrapper');
   const menu = $('.menu');
 
   trigger.on('click', () => {
     menu.toggleClass('active');
+    wrapper.toggleClass('menu-opened');
   });
 };
